ef02331ffd3ba39a7a3601e74b172a050a4bcf4a
ef10res <floreserick10@gmail.com>
2025-07-14 21:05:39 -0600

    first try

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Globalization;
+using System.Collections.Generic;
 public static class Arrays
 {
     /// <summary>
@@ -6,14 +9,31 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    /// I will let the user supply the input for the function then will run the function.
+    Debug.WriteLine("Please enter a number greater than 0 to get multiples:");
+    static double number = Console.ReadLine();
+
+    Debug.WriteLine("Please enter the number of multiples you need: ");
+    static int length = Console.ReadLine();
+    List<double> list = MultiplesOf(number, length);
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-
-        return []; // replace this return statement with your own
+        // get the input from the user and test if it is greater than 0 after that run a loop to iterate through the number variable
+        // depending on the length size variable to get each of the multiples and will store it in an array of the size.
+        int i = 0;
+        List<double> list = new List<double>();
+        if (number > 0 || number > 1)
+        {
+            while (i <= length) ;
+            list.Add(number);
+            number += number;
+            i++;
+        }
+        return list;// replace this return statement with your own
     }
 
     /// <summary>
