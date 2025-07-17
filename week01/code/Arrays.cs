2a588354977271d719418fbfca22d510db949822
ef10res <floreserick10@gmail.com>
2025-07-16 18:00:40 -0600

    tests

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -10,12 +10,12 @@ public static class Arrays
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     /// I will let the user supply the input for the function then will run the function.
-    Debug.WriteLine("Please enter a number greater than 0 to get multiples:");
-    static double number = Console.ReadLine();
+    //Debug.WriteLine("Please enter a number greater than 0 to get multiples:");
+    //static double number = Console.ReadLine();
 
-    Debug.WriteLine("Please enter the number of multiples you need: ");
-    static int length = Console.ReadLine();
-    List<double> list = MultiplesOf(number, length);
+    //Debug.WriteLine("Please enter the number of multiples you need: ");
+    //static int length = Console.ReadLine();
+    //List<double> list = MultiplesOf(number, length);
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
@@ -24,16 +24,17 @@ public static class Arrays
         // be implemented by another person.
         // get the input from the user and test if it is greater than 0 after that run a loop to iterate through the number variable
         // depending on the length size variable to get each of the multiples and will store it in an array of the size.
-        int i = 0;
+        int i = 1;
         List<double> list = new List<double>();
-        if (number > 0 || number > 1)
+        if (number < 0 || number > 1)
         {
-            while (i <= length) ;
-            list.Add(number);
-            number += number;
-            i++;
+            while (i <= length)
+            {
+                list.Add(number * i);
+                i++;
+            }
         }
-        return list;// replace this return statement with your own
+        return list.ToArray<double>();// replace this return statement with your own
     }
 
     /// <summary>
@@ -49,5 +50,23 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // made a new list that will hold the new shifted list.
+        // take the last "amount" of elements and put them in order at the beginning of the list.
+        // put the remainder of the first list elements into the list
+
+        List<int> numbers = new List<int>();
+
+        for (int i = data.Count - amount; i < data.Count; i++)
+        {
+            numbers.Add(data[i]);
+        }
+
+        for (int i = 0; i < data.Count - amount; i++)
+        {
+            numbers.Add(data[i]);
+        }
+
+        data.Clear();
+        data.AddRange(numbers);
     }
 }
